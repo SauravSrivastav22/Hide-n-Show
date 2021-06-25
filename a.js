970d5e485a0699a9987a1c07c355dbf23eb5475a
Saurav Srivastav <saurav121king@gmail.com>
2021-06-25 21:14:56 +0530

    first commit

---
+++ a.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("p").hide();
+
+    $("#btn1").click(function(){
+         $("#p1").toggle();
+    });
+
+    $("#btn2").click(function(){
+         $("#p2").toggle();
+    });
+});
